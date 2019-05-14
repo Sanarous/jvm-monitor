55814f5d79b38411aa481e71bbefd522de64b696
Keathy <zuoxiang@whut.edu.cn>
2019-05-14 21:12:38 +0800

    首次提交

--- src/main/java/cn/bestzuo/controller/BaseDataController.java
+++ src/main/java/cn/bestzuo/controller/BaseDataController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+
 /**
  * @author bestzuo
  * @date 2019-05-10
